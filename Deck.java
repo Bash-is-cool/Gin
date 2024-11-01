1c25ba041d630df972dfd49d0c99b8a35b48b084
Bash-is-cool <154714089+Bash-is-cool@users.noreply.github.com>
2024-11-01 11:09:28 -0500

    Add files via upload

---
+++ Deck.java
@@ -0,0 +1,39 @@
+
+/**
+ * Deck holds all the cards in a standard 52 card deck
+ *
+ * @author Alex Silverman   
+ * @version 1.0 10/31/2024
+ */
+
+import java.util.Scanner;
+
+public class Deck
+{
+    // instance variables - replace the example below with your own
+    private boolean isFullDeck; //Sees if the deck is dull or if a game is in progress
+    private boolean hasJokers;  //adds jokers to the deck
+    private int game;           //Game number
+    private int points1;         //number of points player 1 has
+    private int points2;         //number of points player 2 has
+    private int endGame;        //Final number of points players have to get to
+
+    /**
+     * Constructor for objects of class Deck
+     */
+    public Deck(boolean deck, boolean jokers, int g, int p1, int p2, int fp)
+    {
+        // initialise instance variables
+        isFullDeck = deck;
+        hasJokers = jokers;
+        game = g;
+        points1 = p1;
+        points2 = p2;
+        endGame = fp;
+    }
+
+    public String toString()
+    {
+        return "Is full deck: " + isFullDeck + "/nHas Jokers: " + hasJokers + "/nGame number: " + game + "/nPlayer 1 has " + points1 + "points /nPlayer 2 has " + points2 + "points /nthe game ends at " + endGame + " points."
+    }
+}
---
+++ Hands.java
@@ -0,0 +1,32 @@
+
+/**
+ * Holds players hands
+ *
+ * @author Alex Silverman
+ * @version 1.0 10/31/2024
+ */
+public class Cards
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Cards
+     */
+    public Cards()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
